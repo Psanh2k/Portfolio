1c00ec4eff28e151f6bca80f8ad5ddff0d111ad7
Psanh2k <sanhdang95@gmail.com>
2023-06-02 16:24:35 +0700

    update code .

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,30 +1,32 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import FrontendLayout from '@/Layouts/Frontend.vue';
-import Hero from '@/Components/Frontend/Hero.vue';
-import Promote from '@/Components/Frontend/Promote.vue';
-import About from '@/Components/Frontend/About.vue';
+import { Head, Link } from "@inertiajs/vue3";
+import FrontendLayout from "@/Layouts/Frontend.vue";
+import Hero from "@/Components/Frontend/Hero.vue";
+import Promote from "@/Components/Frontend/Promote.vue";
+import About from "@/Components/Frontend/About.vue";
+import Services from "@/Components/Frontend/Services.vue";
+import ContactMe from "@/Components/Frontend/ContactMe.vue";
 
 defineProps({
-    skills: Object,
-})
+  skills: Object,
+});
 </script>
 
 <template>
-    <Head title="Welcom to portfolio" />
-    <FrontendLayout>
-        <!-- Hero primary -->
-        <Hero :skills="skills"/>
-        <!-- Promote tail-100 -->
-        <Promote/>
-        <!-- About secondary -->
-        <About/>
-        <!-- Portfolio primary -->
-        <!-- Services secondary-->
-        <!-- Contact primary -->
-    </FrontendLayout>
+  <Head title="Welcom to portfolio" />
+  <FrontendLayout>
+    <!-- Hero primary -->
+    <Hero :skills="skills" />
+    <!-- Promote tail-100 -->
+    <Promote />
+    <!-- About secondary -->
+    <About />
+    <!-- Portfolio primary -->
+    <!-- Services secondary-->
+    <Services />
+    <!-- Contact primary -->
+    <ContactMe/>
+  </FrontendLayout>
 </template>
 
-<style>
-
-</style>
+<style></style>
